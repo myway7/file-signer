36c8ed5d9c9f1c4ae20ec17073ff6d308c7b46aa
fbenpao <1250222077@qq.com>
2021-03-15 17:09:06 +0800

    test

--- test_tool/src/main.rs
+++ test_tool/src/main.rs
@@ -1,3 +1,14 @@
+use signer::key_generate_mnemonic;
 fn main() {
-    println!("Hello, world!");
+    let mne = key_generate_mnemonic();
+    match mne{
+        Ok(m)=>{
+            println!("ok");
+
+        },
+        Err(err)=>{
+            println!("err");
+
+        }
+    }
 }
